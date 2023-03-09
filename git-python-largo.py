37ccd41cc4e817f45deec5bdaad94e70c6612573
LargoMarkJoseph <mjlargo015@gmail.com>
2023-03-09 17:35:00 +0800

    My Educational Background Added

--- git-python-largo.py
+++ git-python-largo.py
@@ -2,3 +2,7 @@
 print("Mark Joseph R. Largo")
 print("Laylay, Boac, Marinduque")
 print("January 5, 2003")
+print("Marinduque State College")
+print("BS Information Technology")
+print("2nd Year")
+print("Section 2A")
